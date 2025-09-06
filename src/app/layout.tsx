b839a652f2db8b225783f43bc9db02a6aab4c893
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-09-05 21:59:59 -0300

    Remove anime list and database features
    Deleted all anime list pages, related API routes, database models, and components for list management and anime details. This refactor removes MongoDB and Mongoose dependencies, and updates package dependencies to latest versions, simplifying the project and removing database functionality.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.scss";
-import Nav from "@/components/layout/Nav/Nav";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Anime List Organizer",
@@ -15,13 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
-        <header>
-          <Nav />
-        </header>
-        {children}
-        <SpeedInsights />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import { useState } from "react";
-import Form, { FormData } from "@/components/layout/forms/Form";
-import Search from "@/components/ui/inputs/search/Search";
-
 const Page = () => {
-  const [initialData, setInitialData] = useState<Partial<FormData> | undefined>(
-    undefined
-  );
-
-  return (
-    <>
-      <main>
-        <Search onResult={setInitialData} />
-        <Form initialData={initialData} />
-      </main>
-    </>
-  );
+  return <h1>Anime List Organizer</h1>;
 };
 
 export default Page;
