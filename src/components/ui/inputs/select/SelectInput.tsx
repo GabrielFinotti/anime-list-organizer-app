fefe1044f59d34a07ff5dd306b9aacbf95203097
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-09-09 19:37:08 -0300

    Update default option value in SelectInput
    Changed the default option value from 'label' to an empty string in the SelectInput component to improve clarity and consistency with form handling.

--- src/components/ui/inputs/select/SelectInput.tsx
+++ src/components/ui/inputs/select/SelectInput.tsx
@@ -8,8 +8,8 @@ type SelectInputProps = {
 
 const SelectInput = ({ name, options, label, ...props }: SelectInputProps) => {
   return (
-    <select id={name} className={style.select} defaultValue="label" {...props}>
-      <option value="label" disabled hidden>
+    <select id={name} className={style.select} {...props}>
+      <option value="" disabled hidden>
         {label}
       </option>
       <option value="">Todos</option>
